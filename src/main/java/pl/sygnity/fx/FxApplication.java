d1923b5d3545502c53d29e175f3789a3abf3f6e5
Kuba Salkowski <j.salkowski@wp.pl>
2022-09-06 00:06:53 +0200

    make it runnable

--- src/main/java/pl/sygnity/fx/FxApplication.java
+++ src/main/java/pl/sygnity/fx/FxApplication.java
@@ -2,8 +2,9 @@ package pl.sygnity.fx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FxApplication {
 
 	public static void main(String[] args) {
