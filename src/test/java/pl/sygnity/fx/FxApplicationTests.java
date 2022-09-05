f1aec9c964862c2cca77dd5ee0224c9b3d01cb1d
Kuba Salkowski <j.salkowski@wp.pl>
2022-09-05 22:20:16 +0200

    initial commit

---
+++ src/test/java/pl/sygnity/fx/FxApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sygnity.fx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
