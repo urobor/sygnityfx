566ab57212742bd6c1b7c1e30ccadccc5909d290
Kuba Salkowski <j.salkowski@wp.pl>
2022-09-06 06:41:37 +0200

    mockito core added - tests fixed

--- src/test/java/pl/sygnity/fx/FxApplicationTests.java
+++ src/test/java/pl/sygnity/fx/FxApplicationTests.java
@@ -3,6 +3,7 @@ package pl.sygnity.fx;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,11 +12,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,22 +33,27 @@ class FxApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Autowired
-	private FxRatesRestController fxRatesRestController;
-
 	@MockBean
 	FxRatesService fxRatesService;
 
-	@Before
-	public void setup() {
-		Mockito.when(fxRatesService.calculateFxRate("usd", "gbp", LocalDate.of(2012, 01, 01), BigDecimal.ONE)).thenReturn(BigDecimal.TEN);
+	@Test
+	void testWeekend() throws Exception {
+		given(this.fxRatesService.calculateFxRate("usd", "gbp", LocalDate.of(2012, 01, 01), BigDecimal.ONE)).willThrow(new ResponseStatusException(BAD_REQUEST, "Only working days"));
+		mockMvc.perform(MockMvcRequestBuilders
+						.get("/api/v1/fxrates/usd/gbp/2012-01-01/1"))
+				.andExpect(status().is4xxClientError())
+				.andExpect(status().reason("Only working days"));
+		Mockito.verify(fxRatesService).calculateFxRate("usd", "gbp", LocalDate.of(2012, 01, 01), BigDecimal.ONE);
 	}
 
 	@Test
-	void testWeekend() throws Exception {
+	void testWeek() throws Exception {
+		given(this.fxRatesService.calculateFxRate("usd", "gbp", LocalDate.of(2012, 01, 02), BigDecimal.ONE)).willReturn(BigDecimal.TEN);
 		mockMvc.perform(MockMvcRequestBuilders
-						.get("/api/v1/fxrates/gbp/usd/2012-01-01/1"))
-				.andDo(print());
+						.get("/api/v1/fxrates/usd/gbp/2012-01-02/1"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("10"));
+		Mockito.verify(fxRatesService).calculateFxRate("usd", "gbp", LocalDate.of(2012, 01, 02), BigDecimal.ONE);
 	}
 
 	@Test
